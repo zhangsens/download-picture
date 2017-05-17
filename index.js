6ebf46b11847cc334f5e2a0448151ff55df9cf60
zhangss <zssqqzss@vip.qq.com>
2017-05-17 21:18:05 +0800

    login and save picture

--- index.js
+++ index.js
@@ -1,73 +1,25 @@
-var http = require("http");
 var https = require("https");
 var fs = require("fs");
-var io = require("cheerio");
-var express = require("express");
-var superagent = require("superagent");
-var jQuery = require("jquery");
+var path = require("path");
+var request = require("request");
 
-var target_url = "https://www.pixiv.net/ranking.php?mode=daily&content=illust";
+var login = require("./lib/login.js");
+var save = require("./lib/save.js");
 
-var fileNum = Math.floor(Math.random() * 100);
-
-https.get(target_url, function(res) {
-    var date = new Date();
-    var day = date.getDate();
-    var mouth = date.getMonth() + 1;
-    var year = date.getFullYear();
-    var html = "";
-
-    res.on("data", function(chunk) {
-        html += chunk;
-    });
-
-    res.on("end", function() {
-        var $ = io.load(html);
-        var content = $("section h2 a");
-        var picture_href = pictureHref($, content);
-
-        fs.writeFile("log/pixiv_rank" + year + mouth + day + "_" + fileNum + ".txt", JSON.stringify(picture_href), "utf-8", function(err) {
-            if (err) throw err;
+fs.exists("cookie/pixiv.txt", function(exists) {
+    if (exists) {
+        //var cookie = fs.readFileSync("cookie/pixiv.txt", "utf-8");
+        fs.readFile("cookie/pixiv.txt", "utf-8", function(err, res) {
+            var cookie = res;
+            console.log(cookie);
         });
-    });
+    } else {
+        login();
+    }
 })
 
-function pictureHref($, content) {
-    var pics = {};
-    content.each(function(index, tag) {
-        pics[index] = {
-            "title": tag.children[0].data,
-            "href": "https://www.pixiv.net/" + tag.attribs.href
-        };
-    });
-    return pics;
-}
-
-function savePicture($, src) {
-    https.get(src, function(res) {
-        var data = "";
-
-        res.setEncoding("binary");
-        res.on("data", function(chunk) {
-            data += chunk;
-        }).on("end", function() {
-            fs.writeFile("picture/test.jpg", data, "binary", function(err) {
-                if (err) throw err;
-            });
-        })
-    })
-}
-
-function saveText($, src) {
-    https.get(src, function(res) {
-        var data = "";
-
-        res.on("data", function(chunk) {
-            data += chunk;
-        }).on("end", function() {
-            fs.writeFile("data/text.txt", data, "utf-8", function(err) {
-                if (err) throw err;
-            });
-        });
-    })
-}
+var urls = [
+    "https://i.pximg.net/img-original/img/2017/05/15/00/26/54/62906806_p0.png",
+    "https://i.pximg.net/img-original/img/2017/05/15/02/29/54/62908861_p0.jpg"
+];
+save(urls);
